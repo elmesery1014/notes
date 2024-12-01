4bdef81d0db89d3befe8161e8898fa488822f8e3
elmesery1014 <melmesery23@gmail.com>
2024-12-01 11:13:04 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Note_app());
+}
+
+class Note_app extends StatelessWidget {
+  const Note_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
