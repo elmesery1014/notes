8e0e30b46d81cf44a1aa76f4627a7629b5219c04
elmesery1014 <melmesery23@gmail.com>
2024-12-01 12:21:29 +0200

    create notes

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class Note_app extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
